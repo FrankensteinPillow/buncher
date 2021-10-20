dea5eb4627931510f6f5d339eaa96d3dfd2f74dd
Ilya Zubov <new.world.participant@gmail.com>
2021-10-20 23:23:21 +0300

    Some type improvements. Isort. Black.

--- buncher/sql.py
+++ buncher/sql.py
@@ -1,16 +1,15 @@
-from typing import List, Dict, Any, Set, Tuple, Optional
+from datetime import date
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 import databases as db
-from databases.core import Database
 import sqlalchemy
-from sqlalchemy import Date, Table
+from databases.core import Database
+from sqlalchemy import Date, Table, and_, func, text
 from sqlalchemy.sql import select
-from sqlalchemy.sql.schema import MetaData, Column
-from sqlalchemy import and_, func, text
+from sqlalchemy.sql.schema import Column, MetaData
 from sqlalchemy.sql.selectable import Select, Subquery
 
-from config import get_config, Config
-from datetime import date
+from config import Config, get_config
 
 CONFIG: Config = get_config()
 NUMERIC_COLS: Set = {"impressions", "clicks", "installs", "spend", "revenue"}
@@ -38,25 +37,26 @@ def _selected_columns(columns: List[str]) -> List[Column]:
     return [dataset.c[name] for name in columns] if columns else []
 
 
-def _filter_by_date(query: Select, dates: Tuple[date]) -> Select:
+def _filter_by_date(query: Select, dates: Tuple[date, date]) -> Select:
     if dates:
-        query: Select = query.where(
-            and_(
-                dataset.c.date >= dates[0], dataset.c.date <= dates[1]
-        ))
+        query = query.where(
+            and_(dataset.c.date >= dates[0], dataset.c.date <= dates[1])
+        )
     return query
+
 
 def _filter_by_value(query: Select, filters: List[Tuple[str, str]]) -> Select:
     for f in filters:
         query = query.where(dataset.c[f[0]] == f[1])
     return query
 
+
 def _select_agregate_columns(
-    columns: List[str], group_columns: List[str]
+    columns: List[str], group_columns: Optional[List[str]]
 ) -> List[Column]:
-    if not group_columns:
+    if group_columns is None:
         return []
-    return _selected_columns(set(columns) & NUMERIC_COLS)
+    return _selected_columns(list(set(columns) & NUMERIC_COLS))
 
 
 def _get_selected_columns(
@@ -68,16 +68,16 @@ def _get_selected_columns(
 async def get_data(
     selected_columns: List[str],
     filters: List[Tuple[str, str]],
-    dates: Tuple[str, str],
-    group_columns: Optional[List[str]],
+    dates: Tuple[date, date],
+    group_columns: List[str],
     column_sort_by: Tuple[str, str],
 ) -> List[Dict[str, Any]]:
     columns: List[str] = [c.name for c in dataset.columns]
     agregate_cols: List[Column] = _select_agregate_columns(
         columns, group_columns
     )
-    group_columns: List[Column] = _selected_columns(group_columns)
-    columns: List[Column] = _selected_columns(columns)
+    group_columns = _selected_columns(group_columns)
+    columns = _selected_columns(columns)
     for c in agregate_cols:
         columns.remove(c)
         columns.append(func.sum(c).label(c.name))
@@ -89,13 +89,13 @@ async def get_data(
     if "CPI" in selected_columns:
         sbqry: Subquery = query.subquery("main_table")
         query = select(
-            *sbqry.columns, (sbqry.c.spend / sbqry.c.installs
-        ).label("CPI"))
+            *sbqry.columns, (sbqry.c.spend / sbqry.c.installs).label("CPI")
+        )
     order: str = column_sort_by[0]
     if column_sort_by[1]:
         order = text(f"{column_sort_by[0]} {column_sort_by[1]}")
     query = query.order_by(order)
-    sbqry: Subquery = query.subquery("all_data")
+    sbqry = query.subquery("all_data")
     query = select(*_get_selected_columns(selected_columns, sbqry.columns))
     rows = await database.fetch_all(query)
     return [dict(r) for r in rows]
